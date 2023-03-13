9d9755403f5afc8a333222f2c1c2e0cbe7143eba
Jeimy Jara <jeimyjara@hotmail.com>
2023-03-13 12:55:14 -0500

    Fixed Menu

--- assets/js/components/showLogin.js
+++ assets/js/components/showLogin.js
@@ -1,16 +1,23 @@
 const showLogin = () => {
     const btnLogin = document.querySelector('.btn__login');
     const login = document.querySelector('.login');
-
+    const menu = document.querySelector('.nav__menu');
     login.addEventListener('click', (e) => {
         if (e.target.closest('.btn__close')) {
             login.classList.remove('show__login');
         }
+        if(e.target.closest('.btn__cart')) {
+            login.classList.remove('show__login');
+        }
+        
     });
 
     btnLogin.addEventListener('click', () => {
         login.classList.toggle('show__login');
+        
     });
+
+
 }
 
 export default showLogin;         
--- assets/js/components/showMenu.js
+++ assets/js/components/showMenu.js
@@ -1,10 +1,13 @@
 const showMenu = () => {
     const nav = document.querySelector('.nav');
     const menu = document.querySelector('.nav__menu');
-
+    const login = document.querySelector('.login');
+    const cart = document.querySelector('.cart');
     nav.addEventListener('click', (e) => {
         if(e.target.closest('.btn__menu')) {
             menu.classList.toggle('show__menu');
+            login.classList.remove('show__login');
+            cart.classList.remove('show__cart');
         }
         if(e.target.closest('.btn__close')) {
             menu.classList.remove('show__menu');
@@ -13,6 +16,22 @@ const showMenu = () => {
         if(e.target.closest('.nav__link')) {
             menu.classList.remove('show__menu');
         }
+        //Si el menu esta abierto y se hace click en el logo, se cierra el menu
+        
+
+        if(e.target.closest('.nav__logo')) {
+            menu.classList.remove('show__menu');
+        }
+        if(e.target.closest('.btn__cart')) {
+            menu.classList.remove('show__menu');
+            login.classList.remove('show__login');
+
+        }
+        if(e.target.closest('.btn__login')) {
+            menu.classList.remove('show__menu');
+            cart.classList.remove('show__cart');
+        }
+
     });
 }
 
